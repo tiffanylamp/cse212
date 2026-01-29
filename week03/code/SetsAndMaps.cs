6225980d4da9d411ab3d0d4d2bfbdcd2c7e42129
tiffanylamp <lalampaatiffany@gmail.com>
2026-01-29 13:17:59 +0300

    problem 1,2,3,4

--- week03/code/SetsAndMaps.cs
+++ week03/code/SetsAndMaps.cs
@@ -202,6 +202,8 @@ public static class SetsAndMaps
         // on those classes so that the call to Deserialize above works properly.
         // 2. Add code below to create a string out each place a earthquake has happened today and its magitude.
         // 3. Return an array of these string descriptions.
+
+        // currently figuring this out 
         return [];
     }
 }
